541c523804363e0ff1526089663a613c80f9411a
Miguel Ángel Arroyo Rosales <miguel@ruidovisual.com>
2017-09-20 15:59:25 -0500

    cintillo top

--- app/app.js
+++ app/app.js
@@ -1,18 +1,17 @@
 document.write((function() {
   var button = '';
 
-  button += '<form name="_xclick" action="https://www.paypal.com/ca/cgi-bin/webscr" method="post">';
+  button += '<style>html{padding-top: 65px}</style>'
+  button += '<h1 style="margin:0 20px 0">Dona a los <a style="color:inherit;" target="_blank" href="https://topos.mx">topos.mx</a><div><small><a style="color:#fff; font-weight: 100; font-size: 1rem" target="_blank" href="https://twitter.com/hashtag/Sismomexico2017?src=tren&data_id=tweet%3A910583997314457600">#sismomexico2017</a></small></div></h1>';
+  button += '<form style="margin-bottom: 0; display:flex; align-items:center; justify-content: space-between;" name="_xclick" action="https://www.paypal.com/ca/cgi-bin/webscr" method="post">';
   button += '<input type="hidden" name="cmd" value="_xclick">';
   button += '<input type="hidden" name="business" value="donativos@brigada-rescate-topos.org">';
   button += '<input type="hidden" name="item_name" value="Donaci&oacute;n a equipo de rescate">';
   button += '<input type="hidden" name="currency_code" value="MXN">';
+  button += '<span style="margin-right: 20px">Haz tu donativo a trav&eacute;s de paypal:</span>'
   button += '<input type="image" src="https://www.paypal.com/en_US/i/btn/x-click-butcc-donate.gif" border="0" name="submit" alt="Dona mediante PayPal">';
   button += '</form>';
+  var wrapper = '<div style="box-sizing: border-box; position: fixed; top: 0; left: 0; width: 100%; display:flex; justify-content: space-between; align-items: center; background-color:#9e1504; font-family:sans-serif; color:#fff; padding:10px;">' + button  +'</div>';
 
-  var anchor = '';
-  anchor += '<a target="_blank" href="https://topos.mx">Brigada de Rescate Topos Tlaltelolco, A.C.</a>';
-
-  var wrapper = '<div>' + button + ' -- ' + anchor +'</div>';
-
-  return wrapper;
+  return wrapper
 })());
